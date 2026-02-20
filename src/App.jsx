188f3de10b284229f60f6c183a85354f2ba29fee
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-02-20 16:47:53 +0500

    add pircing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Features from "./sections/features";
 import Hero from "./sections/Hero";
+import Pricing from "./sections/Pricing";
 
 function App() {
   return (
     <div className="bg-[#0B0F19] text-white min-h-screen">
       <Hero />
       <Features />
+      <Pricing />
     </div>
   );
 }
