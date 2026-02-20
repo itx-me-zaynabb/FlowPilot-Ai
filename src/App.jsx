c34f1b48c529a78bedf3e93039124b041d543be6
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-02-20 16:09:11 +0500

    add plugins

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+
+function App() {
+  return (
+    <div className="bg-black text-white overflow-hidden">
+      <Hero />
+      <Features />
+    </div>
+  );
+}
+
+export default App;
