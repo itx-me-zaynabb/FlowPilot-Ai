e25601bd4825daf5ba67c46a8ffe05533c29808e
itx-me-zaynabb <zainabfattimaa@gmai.com>
2026-02-20 17:26:55 +0500

    right p tag

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="py-12 bg-[#0A0D17] text-gray-400 text-center">
+      <p>© 2026 FlowPilot AI. All rights reserved.</p>
+      <div className="flex justify-center gap-6 mt-4">
+        <a href="#">Twitter</a>
+        <a href="#">LinkedIn</a>
+        <a href="#">GitHub</a>
+      </div>
+    </footer>
+  );
+}
